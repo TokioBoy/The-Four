fefc902309941dc9a4a0a1434dc84a2cab90e06d
Vlad <70889664+TokioBoy@users.noreply.github.com>
2022-06-21 12:57:26 +0200

    upd

--- html5-template-master/app.js
+++ html5-template-master/app.js
@@ -1,5 +1,16 @@
 (() => {
 
     
+ const spring = document.querySelector('#spring');
+
+ const springArticle = document.querySelector('#spring>artical');
+
+ spring.addEventListener('click', event => {    
+    console.log('spring has been clicked');          
+        springArticle.classList.toggle('inviewport');
+ });
+
+
+
 
 })();
